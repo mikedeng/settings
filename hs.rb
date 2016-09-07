bd2dd68262c4ee03cc4c89108d01bba6e04813b4
DengMing <dengm@udesk.cn>
2016-09-07 13:24:07 +0800

    add zsh

--- hs.rb
+++ hs.rb
@@ -6,5 +6,9 @@ Hs = {
  		{
  		 "/home/" + login + "/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap" => 1,
  		 "/home/" + login + "/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"   => 1,
- 		}
+ 		},
+ "zsh" =>      {
+		
+                 "/home/" + login + "/.oh-my-zsh/plugins/common-aliases/common-aliases.plugin.zsh"         => 1,
+		}
 }
